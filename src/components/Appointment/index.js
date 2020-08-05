1d37d3efe2ecdf68a7d81db8475a9fad98184193
Margarita Struts <margarita.struts@me.com>
2020-08-05 16:56:12 -0400

    Completed Displaying Appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Fragment } from 'react'
 import "./styles.scss"
 
 import Empty from "./Empty";
@@ -7,7 +7,17 @@ import Header from "./Header";
 
 
 export default function Appointment (props) {
-    return (
-        <article className="appointment"></article>
+    let render;
+    props.interview ? render = <Show student = {props.interview.student} interviewer = {props.interview.interviewer }/> : render = <Empty/>
+
+     return (
+            <Fragment>
+                <Header 
+                    time = {props.time}
+                />
+                <article className="appointment">
+                    {render}
+                </article>
+            </Fragment>
     )
 }
